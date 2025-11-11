f8124e27a7cc6af5427bbf4c01577e60923add38
KARTHIK <122384867+KARTHIK522001@users.noreply.github.com>
2025-11-11 23:29:47 +0530

    Change output message from 'third' to 'forth'

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello world this is my first poll scm job");
-         System.out.println("third commit of the code");
+         System.out.println("forth commit of the code");
     }
 } 
