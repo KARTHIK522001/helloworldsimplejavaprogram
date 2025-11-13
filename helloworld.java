6d34b7fb25fd737e3dce8f77664c1fa892c7ff4f
KARTHIK <122384867+KARTHIK522001@users.noreply.github.com>
2025-11-13 23:43:32 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,4 @@
-class helloworldsimplejavaprogram
-{  
+class helloworld{  
     public static void main(String args[]){  
      System.out.println("Hello world this is my first poll scm job");
          System.out.println("THIS IS MY NEW COMMIT ON DAY 13 with pipline syntax groovy build");
