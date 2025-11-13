0b5c9a39228405fa42ed12e5573ec528bca89d80
KARTHIK <122384867+KARTHIK522001@users.noreply.github.com>
2025-11-13 21:02:34 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello world this is my first poll scm job");
-         System.out.println("forth commit of the code");
+         System.out.println("THIS IS MY NEW COMMIT ON DAY 13");
+        
     }
 } 
