0aef0fef2ffe74726edd9df54d2b3c977b612be8
KARTHIK <122384867+KARTHIK522001@users.noreply.github.com>
2025-11-16 11:53:13 +0530

    Update commit message in helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,7 @@
 class helloworld{  
     public static void main(String args[]){  
      System.out.println("Hello world this is my first poll scm job");
-         System.out.println("THIS IS MY NEW COMMIT ON DAY 13 with pipline syntax groovy build");
+         System.out.println("THIS IS MY NEW COMMIT ON DAY 16 with master slave setup");
         
     }
 } 
